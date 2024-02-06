a425bbd146171d1288133cf1d8671aa2d6789a50
Gustavo Nascimento <gustavohn2017@gmail.com>
2024-02-06 19:43:13 -0300

    updating JS form inputs

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 //Lê os inputs dos botões. Não é recomendável deixar como variáveis globais em projetos mais relevantes.
-var buttonNovoContato = document.getElementById('buttonNovoContato')
-var buttonCancelar = document.getElementById('buttonCancelar')
+var buttonNovoContato = document.getElementById('buttonNovoContato');
+var buttonCancelar = document.getElementById('buttonCancelar');
 var novoContato= document.getElementById('novoContato');
-
+var inputNomeContato = document.getElementById('nomeContato');
+var inputNumeroContato = document.getElementById('numeroContato');
+var inputEmailContato = document.getElementById('emailContato');
 
 
 function mostrarFormContato(){
@@ -23,45 +25,23 @@ buttonCancelar.addEventListener('click', cancelar)
 //lê o botão submit
 const form = document.getElementById("form-contato")
 //Função que previne o reload ao enviar o formulário pelo botão submit
-form.addEventListener('submit', function(event){
-    let formValidado = false;
+function salvarNovoContato(event){
     event.preventDefault();
 
-    const nome = document.getElementById("nome");
-    const numero = document.getElementById("numero");
-    const email = document.getElementById("email");
-    const descricao = document.getElementById("descricao");
-    const sucess = alert("contato cadastrado com sucesso!")
+    var inputNomeContato = inputNomeContato.value;
+    var inputNumeroContato = inputNumeroContato.value;
+    var inputEmailContato =inputEmailContato.value;
+    var sucess = alert("contato cadastrado com sucesso!")
 
 
     formValidado= validaNome(nome.value)
      if (formValidado){
         alert(sucess);
-        nome.value = '';
-        numero.value = '';
-        email.value = '';
+        inputNomeContato.value = '';
+        inputNumeroContato.value = '';
+        inputEmailContato.value = '';
         descricao.value = '';
     } else{
         alert('nome não está completo.')
     }
-})
-
-
-function validaNome(nome){
-    const nomeComoArray = nome.split(' ');
-
-
 }
-
-function adicionar() {
-    const nome = document.getElementById("nome").value;
-    const numero = document.getElementById("numero").value;
-    const email = document.getElementById("email").value;
-    const descricao = document.getElementById("descricao").value;
-  
-    const listaContatos = document.getElementById("contatos");
-    const novoContato = document.createElement("li");
-    novoContato.innerHTML = `${nome} - ${numero} - ${email} - ${descricao}`;
-    listaContatos.appendChild(novoContato);
-  }
-  
